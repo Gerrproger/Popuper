3cc7a878e858fad48dd64a0f5262f8a049505c47
Evgenii D <gerrproger@gmail.com>
2014-07-14 03:25:04 +0400

    New cool features added

--- popuper.js
+++ popuper.js
@@ -1,6 +1,6 @@
 (function(){
 window.popuper = {
-init: function(id, shift){
+init: function(id, shift, scroll, noclose){
 	    var self = this;
 	    $('.popuper').off().click(function(e){
 		   e.preventDefault();
@@ -10,23 +10,31 @@ init: function(id, shift){
 		   e.preventDefault();
 		   self.close();
 	    });
-	    if(id) self.open(id, shift);
+	    if(id) self.open(id, shift, scroll, noclose);
 },
 close: function(){
 		$('.popup-layout').fadeOut(100, function(){ $(this).remove(); });
 		$('.popup').fadeOut(100);
+        this.opened = false;
 },
-open: function(id, shift){
-	    var self = this;
-	    self.close();
-	    $('<div class="popup-layout"></div>').click(function(){
-			self.close();
-		}).appendTo('body').fadeIn(100);
-		var el = $('#' + id);
+open: function(id, shift, scroll, noclose){
+	    var self = this,
+        el = $('#' + id);
         if(shift == undefined) shift = el.data('shift');
+        if(noclose == undefined) noclose = el.data('noclose');
+        if(scroll == undefined) scroll = el.data('scroll');
+	    if(!(self.opened && noclose)){
+            self.close();
+	        $('<div class="popup-layout"></div>').click(function(){
+			    self.close();
+		    }).appendTo('body').fadeIn(100);
+        } console.log(111, shift, scroll);
         if(shift != false) el.css('margin-top', Math.ceil(el.height()/2)*-1);
-        el.fadeIn(100);
+        if(!(self.opened && noclose)) el.fadeIn(100);
+        if(scroll == true) $('html, body').animate({scrollTop: el.offset().top-100}, 300);
+        self.opened = true;
 },
-version: 3
+opened: false,
+version: 5
 }
 })();
